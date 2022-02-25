765419a1c3da51301296987f832732fd943045ca
chak <chakcheung4565@outlook.com>
2022-02-25 16:51:58 +0800

    【配置】element｜VueUse | env

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,39 +1,14 @@
 <script setup lang="ts">
-import { useAgeStore } from '@/store/age'
 import { ref } from 'vue'
+import { useMouse } from '@vueuse/core'
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-const ageStore = useAgeStore()
-console.log(ageStore.chakAge)
+const { x, y } = useMouse()
+console.log(x.value)
+console.log(y.value)
 </script>
 
 <template>
-  <h1 class="test">{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <el-button>测试</el-button>
 </template>
 
 <style scoped lang="scss">
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,42 @@
+/* eslint-disable camelcase */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { join } from 'path'
+
+// element-plus 按需引入配置
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 const resolve = (path: string) => join(__dirname, path)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: './',
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    open: true,
+    https: false,
+    proxy: {},
+  },
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve('src'),
